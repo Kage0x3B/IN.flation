a343dc04fb7ea7ea6e1918a983a02becf8158cf8
Kage0x3B <kage0x3b@outlook.com>
2019-02-17 23:37:20 +0100

    Initial commit

---
+++ include/chat.inc.php
@@ -0,0 +1,44 @@
+<div class="chat">
+    <div class="chat-container">
+        <div class="menu-bar">
+            <div class="back">
+                <i class="zmdi zmdi-arrow-left"></i>
+            </div>
+            <div class="avatar">
+                <img src="https://syscy.de/images/avatar.jpg" alt="Avatar">
+            </div>
+            <div class="name">
+                <span>Moritz Hein</span>
+                <span class="status">online</span>
+            </div>
+            <div class="actions more">
+                <i class="zmdi zmdi-more-vert"></i>
+            </div>
+            <div class="actions attachment">
+                <i class="zmdi zmdi-attachment-alt"></i>
+            </div>
+            <div class="actions">
+                <i class="zmdi zmdi-phone"></i>
+            </div>
+        </div>
+        <div class="conversation">
+            <div class="conversation-container">
+                <div class="message draft-message">
+                    You were hugging an old man with a beard screaming "DUMBLEDORE YOU'RE ALIVE!"
+                    <span class="metadata">
+                        <span class="time"></span>
+                    </span>
+                </div>
+            </div>
+            <div class="conversation-compose">
+                <input class="input-msg" name="input" placeholder="Type a message" autocomplete="off" autofocus />
+                <div class="message-box-hook"></div>
+                <button class="send">
+                    <div class="circle">
+                        <i class="zmdi zmdi-mail-send"></i>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ include/contact_list.inc.php
@@ -0,0 +1,21 @@
+<div class="contact-screen">
+    <div class="menu-bar">
+        <div class="name">
+            <span>Whysapp</span>
+        </div>
+        <div class="actions more">
+            <i class="zmdi zmdi-more-vert"></i>
+        </div>
+    </div>
+    <div class="contact-list">
+        <div class="contact-list-container">
+            <div class="contact">
+                <div class="avatar">
+                    <img src="https://syscy.de/images/avatar.jpg" alt="Avatar">
+                </div>
+                <div class="message-preview">Raphael: In 15 minutes gehts los. Bitte erreichbar lb</div>
+                <div class="time">7:28</div>
+            </div>
+        </div>
+    </div>
+</div>
